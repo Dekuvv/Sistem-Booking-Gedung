1b42c5a62bf55835a11adbab5e20ac7a87117212
Kemal1101 <144076077+Kemal1101@users.noreply.github.com>
2023-09-20 22:12:26 +0700

    Update BookingGedung.java

--- Project Booking Gedung/BookingGedung.java
+++ Project Booking Gedung/BookingGedung.java
@@ -17,27 +17,42 @@ public class BookingGedung {
         System.out.print("Tanggal Pelaksanaan Acara : ");
         tanggalPelaksanaanAcara = input.nextLine();
 
-        System.out.print("Jumlah Tamu Undangan : ");
-        jumlahTamu = input.nextInt();
-
-        System.out.print("Jenis Gedung : ");
-        jenisGedung = input.next();
-
-        System.out.print("Layanan Tambahan : ");
-        layananTambahan = input.next();
-
         System.out.print("Jumlah Tamu Undangan:\n Gedung A = 100 - 200\n Gedung B = 200 - 300\n Gedung C = 300 - 400\n Masukkan Jumlah Tamu:  ");
         jumlahTamu = input.nextInt();
+
+              if (jumlahTamu <=200) {
+            System.out.println("Anda akan menggunakan Gedung A");
+               } else if (200 <= jumlahTamu && jumlahTamu <=300 ) {
+                System.out.println("Anda akan menggunakan Gedung B");
+               }else if (300<= jumlahTamu && jumlahTamu <= 400) {
+                System.out.println("Anda akan menggunakan Gedung C");
+               } else {
+                System.out.println("Yang bener masukin tamunya woi");
+               }
+        
+
+ if (jumlahTamu <=200) {
+            jenisGedung = "Gedung A";
+         }else if (200 <= jumlahTamu && jumlahTamu <=300 ) {
+            jenisGedung = "Gedung B";
+         }else if (300<= jumlahTamu && jumlahTamu <= 400){
+            jenisGedung = "Gedung C";
+         }else {
+           jenisGedung = "coba lagi";
+         }
+
+           System.out.print("Layanan Tambahan : ");
+            layananTambahan = input.next();
 
         boolean pemesananBerhasil = true;
         //OUTPUT
         if (pemesananBerhasil) {
             System.out.println("------ Pemesanan Telah Berhasil------");
-            System.out.println("Nama Pemesan :" + identitasPemesan);
-            System.out.println("Tanggal Pemesanan :" + tanggalPemesanan);
-            System.out.println("Tanggal Pelaksanaan Acara :" + tanggalPelaksanaanAcara);
-            System.out.println("Jumlah Tamu Undangan :" + jumlahTamu);
-            System.out.println("Jenis Gedung :" + jenisGedung);
+            System.out.println("Nama Pemesan : " + identitasPemesan);
+            System.out.println("Tanggal Pemesanan : " + tanggalPemesanan);
+            System.out.println("Tanggal Pelaksanaan Acara : " + tanggalPelaksanaanAcara);
+            System.out.println("Jumlah Tamu Undangan : " + jumlahTamu);
+            System.out.println("Jenis Gedung : " + jenisGedung);
             System.out.println("Layanan Tambahan: " + layananTambahan);
         } else {
             System.out.println("Maaf, Pemesanan yang anda minta tidak dapat kami proses. Silahkan coba lagi nanti.");
