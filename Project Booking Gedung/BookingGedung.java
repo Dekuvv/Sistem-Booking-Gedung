927e67ea71cedd589851bd2e92f3d113639fe49c
Dekuvv <aditbagussadewa85@gmail.com>
2023-11-14 01:54:46 +0700

    Update & Merapikan

--- Project Booking Gedung/BookingGedung.java
+++ Project Booking Gedung/BookingGedung.java
@@ -65,7 +65,7 @@ public class BookingGedung {
             input.nextLine();
             tanggalPelaksanaanAcara = input.nextLine();
             
-            System.out.println("Jumlah Tamu Undangan:  ");
+            System.out.println("Jumlah Tamu Undangan :  ");
             System.out.println("Gedung A = 100 - 200 ");
             System.out.println("Gedung B = 200 - 300 ");
             System.out.println("Gedung C = 300 - 400 ");
